a7bbbc5d516a72eb685ad8f19504ae51974f92a1
thiago gloor <thiago.gloor@xbrain.com.br>
2023-08-15 16:48:51 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package com.devsuperior.hrworker.controller;
 
 import com.devsuperior.hrworker.model.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +20,13 @@ public class WorkerController {
 
     //TODO criar uma service para nao chamar o repository na controller!!!
     //TODO tirar o autowired e colocar a outra anotacao (nao lembro qual e, pesquisar)!!!
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
     @Autowired
     private WorkerRepository repository;
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -28,6 +36,9 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + environment.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
