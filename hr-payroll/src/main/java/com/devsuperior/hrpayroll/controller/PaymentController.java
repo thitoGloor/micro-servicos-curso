d7188e0d8ac614a0d1cf596d7988115dbb7d75ce
thiago gloor <thiago.gloor@xbrain.com.br>
2023-08-16 11:28:50 -0300

    Cria tolerancia hystrix

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrpayroll.controller;
 
 import com.devsuperior.hrpayroll.model.Payment;
 import com.devsuperior.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,15 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{day}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer day) {
         Payment payment = service.getPayment(workerId, day);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long worerId, Integer day) {
+        Payment payment = new Payment("Thiago Gloor", 400.0, day);
         return ResponseEntity.ok(payment);
     }
 }
