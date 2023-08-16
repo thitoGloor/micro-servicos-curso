ec67f0688e46a893f79d0e64f3eaf45114ff91d9
thiago gloor <thiago.gloor@xbrain.com.br>
2023-08-16 11:47:42 -0300

    Cria hr-zuul-api-gateway

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class HrWorkerApplication {
 
-    //TODO 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
     public static void main(String[] args) {
         SpringApplication.run(HrWorkerApplication.class, args);
     }
