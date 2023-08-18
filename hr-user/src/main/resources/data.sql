b8834ede47534ad3c43c5dd6e63496dfafaa8b64
thiago gloor <thiago.gloor@xbrain.com.br>
2023-08-18 10:51:36 -0300

    Cria database seeding

---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO USER (name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO USER (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+
+INSERT INTO ROLE (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO ROLE (role_name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO USER_ROLE (user_id, role_id) VALUES (1, 1);
+INSERT INTO USER_ROLE (user_id, role_id) VALUES (2, 1);
+INSERT INTO USER_ROLE (user_id, role_id) VALUES (2, 2);
