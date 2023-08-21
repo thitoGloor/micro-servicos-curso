e15539c8b7b6967ebd29d1e7c49d1e7a8c405bf5
thiago gloor <thiago.gloor@xbrain.com.br>
2023-08-21 15:46:46 -0300

    Zuul authorization 'commit com erro'

---
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ControllerServerConfig.java
@@ -0,0 +1,35 @@
+package com.devsuperior.hrapigatewayzuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ControllerServerConfig extends ResourceServerConfigurerAdapter {
+
+    private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
+    private static final String[] OPERATOR = { "/hr-worker/**" };
+    private static final String[] ADMIN = { "hr-payroll/**", "/hr-user/**" };
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN).hasAnyRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+}
